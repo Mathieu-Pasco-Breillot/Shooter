a38187bcf8d2c9179557d2081ffabbaaac52056f
Alexandre Liscia <alexandre.liscia@orinoko.fr>
2020-04-10 14:43:22 +0200

    Basic player movement && basic map

---
+++ Assets/Scripts/Com/Kearny/Shooter/Player/Look.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+
+namespace Com.Kearny.Shooter.Player
+{
+    public class Look : MonoBehaviour
+    {
+        public static bool cursorLocked = true;
+
+        public Transform player;
+
+        public Transform playerEyes;
+
+        public float xSensitivity;
+        public float ySensitivity;
+
+        public float minYAngle = -65f;
+        public float maxYAngle = 30f;
+
+        private readonly Quaternion _camCenter = new Quaternion(0, 0, 0, 1);
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            if (cursorLocked)
+            {
+                SetY();
+                SetX();
+            }
+
+            UpdateLockCursor();
+        }
+
+        private void SetY()
+        {
+            var yAngle = Input.GetAxis("Mouse Y") * ySensitivity * Time.deltaTime;
+            var xRotation = Quaternion.AngleAxis(yAngle, -Vector3.right);
+            var delta = playerEyes.localRotation * xRotation;
+
+            var angle = Quaternion.Angle(_camCenter, delta);
+            if (angle > minYAngle && angle < maxYAngle)
+                playerEyes.localRotation = delta;
+        }
+
+        private void SetX()
+        {
+            var yAngle = Input.GetAxis("Mouse X") * xSensitivity * Time.deltaTime;
+            var xRotation = Quaternion.AngleAxis(yAngle, Vector3.up);
+            var delta = player.localRotation * xRotation;
+
+            player.localRotation = delta;
+        }
+
+        void UpdateLockCursor()
+        {
+            if (cursorLocked)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    cursorLocked = false;
+                }
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    cursorLocked = true;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Com/Kearny/Shooter/Player/PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Com.Kearny.Shooter.Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [Header("Player Motor")] [Range(1f, 15f)]
+        public float walkSpeed;
+
+        [Range(1f, 15f)] public float runSpeed;
+        [Range(1f, 15f)] public float jumpForce;
+
+        private CharacterController _characterController;
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            if (Camera.main != null) Camera.main.enabled = false;
+            _characterController = GetComponent<CharacterController>();
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            var horizontalInput = Input.GetAxisRaw("Horizontal");
+            var verticalInput = Input.GetAxisRaw("Vertical");
+
+            var isGrounded = _characterController.isGrounded;
+            var localTransform = transform;
+            Vector3 fwdMovement = isGrounded ? localTransform.forward * verticalInput : Vector3.zero;
+            Vector3 rightMovement = isGrounded ? localTransform.right * horizontalInput : Vector3.zero;
+
+            Console.WriteLine(Input.GetKeyDown(KeyCode.LeftShift));
+
+            var speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
+            _characterController.SimpleMove(Vector3.ClampMagnitude(fwdMovement + rightMovement, 1f) * speed);
+
+            if (_characterController.isGrounded)
+                Jump();
+        }
+
+        private void Jump()
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                StartCoroutine(PerformJumpRoutine());
+            }
+        }
+
+        private IEnumerator PerformJumpRoutine()
+        {
+            var jump = jumpForce;
+
+            do
+            {
+                _characterController.Move(Vector3.up * (jump * Time.deltaTime));
+                jump -= Time.deltaTime;
+                yield return null;
+            } while (!_characterController.isGrounded);
+        }
+    }
+}
